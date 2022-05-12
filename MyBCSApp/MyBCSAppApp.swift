f73adae3fa0e68a552200caab042a1b9d3c8be0f
AnaSavu <anasavu001@gmail.com>
2022-05-12 11:56:20 +0300

    Initial Commit

---
+++ MyBCSApp/MyBCSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBCSAppApp.swift
+//  MyBCSApp
+//
+//  Created by Ana Savu on 5/12/22.
+//
+
+import SwiftUI
+
+@main
+struct MyBCSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
